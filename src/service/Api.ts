da1c598369ac6bcd38a608c482cdc8b3cbdd7827
Yara-Cordero <145566014+Yara-Cordero@users.noreply.github.com>
2024-07-23 14:07:20 +0200

    update api to secure

--- src/service/Api.ts
+++ src/service/Api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 
-const BASE_URL = 'http://ergast.com/api'
+const BASE_URL = 'https://ergast.com/api'
 
 export const defaultAxiosInstance: AxiosInstance = axios.create({
     baseURL: BASE_URL,
